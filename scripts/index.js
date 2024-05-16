a69f4dcdae52f4db3f693cd1fc945f4cbbe61fee
Max Chernov <mycakeee@yandex.ru>
2024-05-16 19:44:59 +0300

    make index js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+initialCards.forEach(elementValue => createWidget(elementValue));
+
+function createWidget(element) {
+const cardTemplate = document.querySelector('#card-template');
+const placesList = document.querySelector('.places__list');
+const cardTemplateContent = cardTemplate.content;
+// клонируем содержимое тега template
+const newCardTemplate = cardTemplateContent.querySelector('.places__item').cloneNode(true);
+
+// наполняем содержимым
+newCardTemplate.querySelector('.card__image').src = element.link;
+newCardTemplate.querySelector('.card__title').textContent = element.name;
+const deleteButton = newCardTemplate.querySelector('.card__delete-button');
+deleteButton.addEventListener('click', function() {
+    deleteCard(newCardTemplate);
+} );
+// отображаем на странице
+placesList.append(newCardTemplate);
+
+function deleteCard() {
+    newCardTemplate.remove();
+};
+
+}
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
