f9f33d205f4546dbf0ed38a4fba5c538deec0069
Max Chernov <mycakeee@yandex.ru>
2024-05-21 16:22:14 +0300

    исправления после ревью

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы Архыза",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "пруд в Челябинске",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Ивановские Дома",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "земля Камчатки",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "железная дорога в лесу",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "берег озера Байкал",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,40 @@
-initialCards.forEach((elementValue) => createWidget(elementValue));
+initialCards.forEach((elementValue) => handleCard(elementValue));
 
-function createWidget(element) {
-  const cardTemplate = document.querySelector("#card-template");
-  const placesList = document.querySelector(".places__list");
-  const cardTemplateContent = cardTemplate.content;
-  // клонируем содержимое тега template
-  const newCardTemplate = cardTemplateContent
-    .querySelector(".places__item")
-    .cloneNode(true);
-  // наполняем содержимым
-  newCardTemplate.querySelector(".card__image").src = element.link;
-  newCardTemplate.querySelector(".card__title").textContent = element.name;
-  const deleteButton = newCardTemplate.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", function () {
-    deleteCard(newCardTemplate);
-  });
-  // отображаем на странице
-  placesList.append(newCardTemplate);
-
-  function deleteCard() {
-    newCardTemplate.remove();
-  }
+function handleCard(element) {
+  
+  const newCardTemplate = getClonableCard();
+  
+  fillCard(newCardTemplate, element);
+  handleDeleteButton(newCardTemplate,function (event) {
+    const cardToDelete = event.target.closest('.card');
+    cardToDelete.remove();
+  })
+ 
+  attachCard(newCardTemplate);
 }
+
+function getClonableCard() {
+  const cardTemplate = document.querySelector("#card-template");
+  const cardTemplateContent = cardTemplate.content;
+  return cardTemplateContent.querySelector(".places__item").cloneNode(true);
+}
+
+function fillCard(newCardTemplate, element) {
+  newCardTemplate.querySelector(".card__image").src = element.link;
+  newCardTemplate.querySelector(".card__image").alt = element.alt;
+  newCardTemplate.querySelector(".card__title").textContent = element.name;
+}
+
+function handleDeleteButton(newCard,funcDelCard) {
+  const deleteButton = newCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click",funcDelCard);
+}
+
+function attachCard(newCardTemplate) {
+  const placesList = document.querySelector(".places__list");
+  placesList.append(newCardTemplate);
+}
+
 
 // @todo: Темплейт карточки
 
